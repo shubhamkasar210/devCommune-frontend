4a6d2c0ab8c4163b25ddf534eafca926c89df3b0
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-15 07:49:55 +0530

    basic layout of the components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <h1 class="text-3xl font-bold">Hello world!</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
