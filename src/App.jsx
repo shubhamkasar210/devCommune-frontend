08b699a1a95a084074a355d5e248882977909d4d
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-17 19:33:38 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
-import axios from "axios";
-axios.defaults.withCredentials = true;
 
 
 function App() {
